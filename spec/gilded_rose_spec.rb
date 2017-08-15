4e216c91419bfed8eef065cf5a0fcb10ecb721da
Ant Barnes <ant@arayb.com>
2017-08-15 20:03:41 +0100

    Complete test suite

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,15 +7,278 @@ RSpec.describe GildedRose do
     let(:initial_quality) { 10 }
     let(:initial_days_remaining) { 5 }
 
-    subject { item.tick }
+    before do
+      item.tick
+    end
 
     context 'with normal item' do
-      let(:name) { 'NORMAL ITEM'}
+      let(:name) { 'NORMAL ITEM' }
+
+      it 'updates days_remaining correctly' do
+        expect(item.days_remaining).to eq(initial_days_remaining - 1)
+      end
 
       context 'before sell by date' do
         it 'updates quality correctly' do
-          subject
           expect(item.quality).to eq(initial_quality - 1)
+        end
+      end
+
+      context 'on sell by date' do
+        let(:initial_days_remaining) { 0 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality - 2)
+        end
+      end
+
+      context 'after sell by date' do
+        let(:initial_days_remaining) { -10 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality - 2)
+        end
+      end
+
+      context 'of zero quality' do
+        let(:initial_quality) { 0 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(0)
+        end
+      end
+    end
+
+    context 'aged brie' do
+      let(:name) { 'Aged Brie' }
+
+      it 'updates days_remaining correctly' do
+        expect(item.days_remaining).to eq(initial_days_remaining - 1)
+      end
+
+      context 'before sell by date' do
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 1)
+        end
+
+        context 'with max quality' do
+          let(:initial_quality) { 50 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'before on sell by date' do
+        let(:initial_days_remaining) { 0 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 2)
+        end
+
+        context 'near max quality' do
+          let(:initial_quality) { 49 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(50)
+          end
+        end
+
+        context 'with max quality' do
+          let(:initial_quality) { 50 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'after sell by date' do
+        let(:initial_days_remaining) { -10 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 2)
+        end
+
+        context 'with max quality' do
+          let(:initial_quality) { 50 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+    end
+
+
+    context 'Sulfuras' do
+      let(:name) { 'Sulfuras, Hand of Ragnaros' }
+
+      it 'updates days_remaining correctly' do
+        expect(item.days_remaining).to eq(initial_days_remaining)
+      end
+
+      context 'before sell by date' do
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality)
+        end
+      end
+
+      context 'on sell by date' do
+        let(:initial_days_remaining) { 0 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality)
+        end
+      end
+
+      context 'after sell by date' do
+        let(:initial_days_remaining) { -10 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality)
+        end
+      end
+    end
+
+    context 'Backstage pass' do
+      let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+
+      it 'updates days_remaining correctly' do
+        expect(item.days_remaining).to eq(initial_days_remaining - 1)
+      end
+
+      context 'long before sell by date' do
+        let(:initial_days_remaining) { 11 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 1)
+        end
+      end
+
+      context 'medium close to  sell by date (upper bound)' do
+        let(:initial_days_remaining) { 10 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 2)
+        end
+
+        context 'at max quality' do
+          let(:initial_quality) { 50 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'very close to  sell by date (upper bound)' do
+        let(:initial_days_remaining) { 5 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 3)
+        end
+
+        context 'at max quality' do
+          let(:initial_quality) { 50 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'very close to  sell by date (lower bound)' do
+        let(:initial_days_remaining) { 1 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(initial_quality + 3)
+        end
+
+        context 'at max quality' do
+          let(:initial_quality) { 50 }
+
+          it 'updates quality correctly' do
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'after sell by date' do
+        let(:initial_days_remaining) { 0 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(0)
+        end
+      end
+
+      context 'after sell by date' do
+        let(:initial_days_remaining) { -10 }
+
+        it 'updates quality correctly' do
+          expect(item.quality).to eq(0)
+        end
+      end
+    end
+
+    context 'Conjured item' do
+      let(:name) { 'Conjured Mana Cake' }
+
+      it 'updates days_remaining correctly' do
+        skip
+        expect(item.days_remaining).to eq(initial_days_remaining - 1)
+      end
+
+      context 'before sell by date' do
+        it 'updates quality correctly' do
+          skip
+          expect(item.quality).to eq(initial_quality - 2)
+        end
+
+        context 'at zero quality' do
+          let(:initial_quality) { 0 }
+
+          it 'updates quality correctly' do
+            skip
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'on sell by date' do
+        let(:initial_days_remaining) { 0 }
+
+        it 'updates quality correctly' do
+          skip
+          expect(item.quality).to eq(initial_quality - 4)
+        end
+
+        context 'at zero quality' do
+          let(:initial_quality) { 0 }
+
+          it 'updates quality correctly' do
+            skip
+            expect(item.quality).to eq(initial_quality)
+          end
+        end
+      end
+
+      context 'after sell by date' do
+        let(:initial_days_remaining) { -10 }
+
+        it 'updates quality correctly' do
+          skip
+          expect(item.quality).to eq(initial_quality - 4)
+        end
+
+        context 'at zero quality' do
+          let(:initial_quality) { 0 }
+
+          it 'updates quality correctly' do
+            skip
+            expect(item.quality).to eq(initial_quality)
+          end
         end
       end
     end
